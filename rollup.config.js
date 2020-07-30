a3069b39206b9fdf9a6fef7aaa0947731e36da68
Dariel Vicedo <darielvicedo@gmail.com>
2020-07-30 15:20:49 -0400

    Require and configure Rollup

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import babel from '@rollup/plugin-babel';
+
+const pkg = require('./package.json');
+const year = new Date().getFullYear();
+
+export default {
+    input: './index.js',
+    output: {
+        banner: `/*!
+ * Gentle Guide v${pkg.version}
+ * Copyright (c) ${year} ${pkg.author}
+ * Licensed under ISC (https://github.com/devmanextensions/gentle-guide/blob/master/LICENSE)
+ */`,
+        file: 'dist/js/gentleguide.js',
+        format: 'esm',
+        name: 'gentleguide',
+    },
+    plugins: [babel({ babelHelpers: 'external', exclude: 'node_modules/**' })],
+};
