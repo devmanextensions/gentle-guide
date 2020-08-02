d320010b7939893843afabcad84af215778e3d56
Dariel Vicedo <darielvicedo@gmail.com>
2020-08-02 18:16:02 -0400

    Initial features

---
+++ index.js
@@ -0,0 +1,182 @@
+import { GCard } from './src/Card';
+
+export default class GGuide {
+    /**
+     * @param {object} cards the cards of the guide
+     * @param {object} options the client options
+     *
+     * @return {GGuide}
+     */
+    constructor(cards, options = {}) {
+        const defaults = {
+            firstCard: 0,
+            showPrev: true,
+            canFinish: true,
+        };
+
+        this.options = Object.assign({}, defaults, options);
+
+        this.currentStep = this.options.firstCard;
+        this.steps = this.createSteps(cards);
+
+        this.onNext = this.onNext.bind(this);
+        this.onPrev = this.onPrev.bind(this);
+        this.onFinish = this.onFinish.bind(this);
+
+        return this;
+    }
+
+    /**
+     * Next card event callback.
+     */
+    onNext = () => {
+        this.showNext();
+    };
+
+    /**
+     * Prev card event callback.
+     */
+    onPrev = () => {
+        this.showPrev();
+    };
+
+    /**
+     * Finish guide event callback.
+     */
+    onFinish = () => {
+        this.end();
+    };
+
+    /**
+     * Create the steps based on the client's cards.
+     *
+     * @param {Object} cards the card object of the client
+     * @return {Object[]}
+     */
+    createSteps(cards) {
+        const stepList = [];
+
+        for (let i = 0; i < cards.length; i++) {
+            const step = {};
+
+            step.anchor = this.getAnchor(cards[i].selector);
+            step.position = cards[i].position;
+            step.content = cards[i].content;
+
+            step.prev = i > 0 ? stepList[i - 1] : null;
+            step.next = null;
+            if (step.prev !== null) {
+                step.prev.next = step;
+            }
+
+            const cardOptions = typeof cards[i].options !== 'undefined' ? cards[i].options : {};
+
+            const canFinish =
+                typeof cardOptions.canFinish !== 'undefined' ? cardOptions.canFinish : true;
+
+            const defaults = {
+                showPrev: this.options.showPrev ? step.prev !== null : false,
+                canFinish: this.options.canFinish ? canFinish : false,
+                cardStep: i,
+                onNext: this.onNext,
+                onPrev: this.onPrev,
+                onFinish: this.onFinish,
+            };
+
+            const options = Object.assign({}, defaults, cards[i].options);
+
+            const card = new GCard(step.anchor, step.position, step.content, options);
+            step.card = card;
+
+            stepList.push(step);
+        }
+
+        return stepList;
+    }
+
+    /**
+     * Starts the guide.
+     *
+     * @return {GGuide}
+     */
+    begin() {
+        this.drawStep(this.steps[this.currentStep]);
+
+        return this;
+    }
+
+    /**
+     * Ends the guide.
+     *
+     * @return {GGuide}
+     */
+    end() {
+        this.removeStep(this.steps[this.currentStep]);
+        this.currentStep = this.options.firstCard;
+
+        return this;
+    }
+
+    /**
+     * Display the next card or end the guide.
+     */
+    showNext() {
+        const currentStep = this.steps[this.currentStep];
+
+        if (this.steps[this.currentStep].next !== null) {
+            this.removeStep(currentStep);
+            this.drawStep(currentStep.next);
+
+            this.currentStep += 1;
+        } else {
+            this.currentStep = this.options.firstCard;
+            this.removeStep(currentStep);
+        }
+    }
+
+    /**
+     * Display the previous card if apply.
+     */
+    showPrev() {
+        const currentStep = this.steps[this.currentStep];
+
+        if (this.steps[this.currentStep].prev !== null) {
+            this.removeStep(currentStep);
+            this.drawStep(currentStep.prev);
+
+            this.currentStep -= 1;
+        }
+    }
+
+    /**
+     * Draws the card corresponding to the step.
+     *
+     * @param {Object} step the step that owns the card to draw
+     */
+    drawStep(step) {
+        step.anchor.scrollIntoView(true);
+        document.body.append(step.card.getElement());
+
+        step.card.setCardInPosition();
+    }
+
+    /**
+     * Removes the card corresponding to the step.
+     *
+     * @param {Object} step the step that owns the card to remove
+     */
+    removeStep(step) {
+        step.card.getElement().remove();
+    }
+
+    /**
+     * Select a DOM element based on selector.
+     *
+     * @param {String} selector
+     */
+    getAnchor(selector) {
+        const anchor = document.getElementById(selector);
+
+        return anchor;
+    }
+}
---
+++ src/Card.js
@@ -0,0 +1,159 @@
+export class GCard {
+    /**
+     * @param {Object} anchor the element to point to
+     * @param {String} position the position of the card, relative to anchor
+     * @param {String} content the text of the card
+     * @param {Object} options the options
+     */
+    constructor(anchor, position, content, options = {}) {
+        const defaults = {
+            showPrev: true,
+            canFinish: true,
+        };
+
+        this.anchor = anchor;
+        this.position = position;
+        this.content = content;
+
+        this.options = Object.assign({}, defaults, options);
+
+        this.element = this.createElement();
+    }
+
+    /**
+     * Gets the DOM element representing the card.
+     */
+    getElement() {
+        return this.element;
+    }
+
+    /**
+     * Creates the DOM element representing the card.
+     */
+    createElement() {
+        const card = document.createElement('div');
+        card.className = 'gg-card';
+
+        const content = document.createElement('div');
+        content.className = 'gg-card-content';
+        content.textContent = this.content;
+
+        const footer = document.createElement('div');
+        footer.className = 'gg-card-footer';
+
+        const footerLeft = document.createElement('div');
+        footerLeft.className = 'gg-card-footer-left';
+
+        const footerCenter = document.createElement('div');
+        footerCenter.className = 'gg-card-footer-center';
+
+        const footerRight = document.createElement('div');
+        footerRight.className = 'gg-card-footer-right';
+
+        footer.append(footerLeft);
+        footer.append(footerCenter);
+        footer.append(footerRight);
+
+        if (this.options.showPrev) {
+            const prev = document.createElement('a');
+            prev.setAttribute('role', 'button');
+            prev.textContent = '<< prev';
+            prev.addEventListener('click', this.options.onPrev, false);
+
+            footerLeft.append(prev);
+        }
+
+        if (this.options.canFinish) {
+            const finish = document.createElement('a');
+            finish.setAttribute('role', 'button');
+            finish.className = 'gg-card-action-finish';
+            finish.textContent = 'end it now';
+
+            finish.addEventListener('click', this.options.onFinish, false);
+
+            footerCenter.append(finish);
+        }
+
+        const next = document.createElement('a');
+        next.setAttribute('role', 'button');
+        next.className = 'gg-card-action-next';
+        next.textContent = 'Got it!';
+        next.addEventListener('click', this.options.onNext, false);
+
+        footerRight.append(next);
+
+        const arrow = document.createElement('div');
+        arrow.className = 'gg-card-arrow';
+
+        card.append(content);
+        card.append(footer);
+        card.append(arrow);
+
+        return card;
+    }
+
+    /**
+     * Gets the coordinates of the anchor in the page.
+     *
+     * @return {Object}
+     */
+    getAnchorCoordinates() {
+        const position = this.anchor.getBoundingClientRect();
+
+        return {
+            top: position.top + window.pageYOffset,
+            left: position.left + window.pageXOffset,
+        };
+    }
+
+    /**
+     * Add classes and styles to positioning the card element.
+     */
+    setCardInPosition() {
+        const anchorCoordinates = this.getAnchorCoordinates();
+
+        switch (this.position) {
+            case 'top':
+            default:
+                this.element.className += ' gg-card-top';
+                this.element.style.left =
+                    anchorCoordinates.left -
+                    this.element.offsetWidth / 2 +
+                    this.anchor.offsetWidth / 2 +
+                    'px';
+                this.element.style.top =
+                    anchorCoordinates.top - this.element.offsetHeight - 15 + 'px';
+                break;
+            case 'right':
+                this.element.className += ' gg-card-right';
+                this.element.style.left =
+                    anchorCoordinates.left + this.anchor.offsetWidth + 15 + 'px';
+                this.element.style.top =
+                    anchorCoordinates.top -
+                    this.element.offsetHeight / 2 +
+                    this.anchor.offsetHeight / 2 +
+                    'px';
+                break;
+            case 'bottom':
+                this.element.className += ' gg-card-bottom';
+                this.element.style.left =
+                    anchorCoordinates.left -
+                    this.element.offsetWidth / 2 +
+                    this.anchor.offsetWidth / 2 +
+                    'px';
+                this.element.style.top =
+                    anchorCoordinates.top + this.anchor.offsetHeight + 15 + 'px';
+                break;
+            case 'left':
+                this.element.className += ' gg-card-left';
+                this.element.style.left =
+                    anchorCoordinates.left - this.element.offsetWidth - 15 + 'px';
+                this.element.style.top =
+                    anchorCoordinates.top -
+                    this.element.offsetHeight / 2 +
+                    this.anchor.offsetHeight / 2 +
+                    'px';
+                break;
+        }
+    }
+}
